440cd6ffbdf50e6560e4139695bf81a375cbac8d
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2025-02-12 12:15:51 +0600

    first commit

---
+++ var-let-const.js
@@ -0,0 +1,11 @@
+// var: no reason to use var
+// let: Allow it to reassign
+//const: Do not allow it to reassign
+
+const money =25;
+const rich = money + 50;
+console.log(rich);
+
+let count =0;
+count =count+10;
+console.log(count);
