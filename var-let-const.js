9713f928d4bc17f80d7217a4a253471353e522ce
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2025-02-22 15:35:36 +0600

    added let,var,const

--- var-let-const.js
+++ var-let-const.js
@@ -9,3 +9,22 @@ console.log(rich);
 let count =0;
 count =count+10;
 console.log(count);
+
+// Const er values Reassign kora jabe na But let and Var er values reassign kora jabe.
+
+// Object
+// Reassign hobena:Example-
+const student ={
+name :"maya pakhi",
+class: 12
+}
+student ={name:"Abdullah"}
+console.log(student)
+
+// Reassign hobe:Example-
+let students ={
+    name: 'Abdullah',
+    class: 13
+}
+student ={name:'Raihan',class:11}
+console.log(students)
